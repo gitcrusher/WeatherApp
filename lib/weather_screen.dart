2cf07c28e9cad68cdf8c8c5b24b75755c267c434
gitcrusher <aayushguptasoni@gmail.com>
2024-08-06 00:22:22 +0530

    commit

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class WeatherScreen extends StatelessWidget {
   const WeatherScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Weather App',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        actions: [
+          GestureDetector(
+            onTap: () {
+              print('refresh');
+            },
+            child: Icon(Icons.refresh),
+          ),
+        ],
+      ),
+    );
   }
 }
