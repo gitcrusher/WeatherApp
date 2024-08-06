5d4f71bd8bf154ba291d31b40a223a08dd4e9c0a
gitcrusher <aayushguptasoni@gmail.com>
2024-08-06 22:12:01 +0530

    COMMIT

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,5 +1,6 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class WeatherScreen extends StatelessWidget {
   const WeatherScreen({super.key});
@@ -14,13 +15,86 @@ class WeatherScreen extends StatelessWidget {
         ),
         centerTitle: true,
         actions: [
-          GestureDetector(
-            onTap: () {
-              print('refresh');
-            },
-            child: Icon(Icons.refresh),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.refresh),
           ),
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            // main card -------->
+
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        top: 16,
+                        bottom: 16,
+                      ),
+                      child: Column(
+                        children: [
+                          Text(
+                            '300°F',
+                            style: TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 16,
+                          ),
+                          Icon(
+                            Icons.cloud,
+                            size: 64,
+                          ),
+                          const SizedBox(
+                            height: 16,
+                          ),
+                          Text(
+                            'Rain',
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            //weather forecast cards -------->
+
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: const Placeholder(
+                fallbackHeight: 150,
+              ),
+            ),
+
+            //additional information -------->
+
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: const Placeholder(
+                fallbackHeight: 150,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
